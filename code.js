297aab1f0299ee53a4b75fcd6159d3c74d715c41
VictorFuecoco <liwiktor69@gmail.com>
2025-02-03 20:32:20 +0100

    Update code.js

--- code.js
+++ code.js
@@ -10,12 +10,12 @@ function showSubButtons(index) {
 
 function showInstructions(option) {
     const instructionsText = {
-        1: "Instrukcja dla opcji 1A",
-        2: "Instrukcja dla opcji 1B",
-        3: "Instrukcja dla opcji 2A",
-        4: "Instrukcja dla opcji 2B",
-        5: "Instrukcja dla opcji 3A",
-        6: "Instrukcja dla opcji 3B"
+        1: "Inspect where and when the card twists, check each area one by one seperately until you narrow the issue down, remember it could be multiple factors at play",
+        2: "Check the pallet how sticky it is and weather it's not being held down, make sure the air lifts and seperates the sheets correctly and make sure it't not bumping into anything",
+        3: "Clean the belts, follow the 7 steps of setting up a feeder",
+        4: "Check with a sheet of paper how it cuts and the pressure",
+        5: "Make sure the pallet is snug and perfectly square inside feeder, press start and check quality",
+        6: "adjust gently little by little with the distance"
     };
 
     document.getElementById("instructions").innerText = instructionsText[option] || "Brak instrukcji";
